12ae82c7639711dd84aad341c863b6c903fd4833
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-04-15 16:28:15 +0100

    setting window height, width, caption

---
+++ snake_game.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import random
+
+#initializing pygame
+pygame.init()
+
+#setting window height and width
+window_width = 600
+window_height = 800
+
+caption = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("SNAKE GAME")
