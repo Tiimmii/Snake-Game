8a1bec226c1ace5f7c7892a3038a5508e2112ded
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-04-15 17:28:43 +0100

    declaring basic variables

--- snake_game.py
+++ snake_game.py
@@ -9,5 +9,21 @@ pygame.init()
 window_width = 600
 window_height = 800
 
-caption = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("SNAKE GAME")
+# defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("SNAKE GAME BY TIMMI")
+
+#setting timer
+timer = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 10
+
+display_font = pygame.font.SysFont("arial",30,"bold")
+score_font = pygame.font.SysFont("arial",45,"bold")
